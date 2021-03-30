c958543c8a4b6b07b2ee1dadd65a8b728e5568da
orthegavalentin <iravadu@gmail.com>
2021-03-30 22:40:33 +0200

    it is a marathon

--- src/com/company/ReadXMLFileJava.java
+++ src/com/company/ReadXMLFileJava.java
@@ -14,6 +14,7 @@ import java.util.ArrayList;
 
 public class ReadXMLFileJava {
     public static void main(String[] args) {
+        System.out.println("It is a godamn marathon baby");
 
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
         try {
